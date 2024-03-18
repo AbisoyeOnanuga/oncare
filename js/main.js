2cd0bab63e83dca31a838815c46dfa73715ad6a8
Abisoye Onanuga <102636953+dottymatrix@users.noreply.github.com>
2024-03-17 22:28:44 -0400

    update index style js

---
+++ js/main.js
@@ -0,0 +1,123 @@
+/*===== MENU SHOW =====*/ 
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*==================== TYPING EFFECT ====================*/
+const words = ['voice-notes', 'text-notes', 'live monitoring'];
+const typingContainer = document.querySelector('.typing-container');
+let wordIndex = 0;
+let letterIndex = 0;
+let currentWord = '';
+let isDeleting = false;
+
+function type() {
+  if (isDeleting) {
+    currentWord = words[wordIndex].substring(0, currentWord.length - 1);
+  } else {
+    currentWord = words[wordIndex].substring(0, letterIndex + 1);
+  }
+  typingContainer.textContent = currentWord;
+  letterIndex = isDeleting ? letterIndex - 1 : letterIndex + 1;
+
+  if (!isDeleting && currentWord === words[wordIndex]) {
+    // Word is complete, pause before starting to delete
+    setTimeout(() => { isDeleting = true; }, 2000);
+  } else if (isDeleting && currentWord === '') {
+    // Word is deleted, move to the next word
+    isDeleting = false;
+    wordIndex = (wordIndex + 1) % words.length;
+  }
+
+  let typingSpeed = 200 - Math.random() * 100; // Speed up typing
+  if (isDeleting) typingSpeed /= 2; // Speed up deleting
+
+  setTimeout(type, typingSpeed);
+}
+
+// Start typing effect
+type();
+/*==================== IMAGE SLIDESHOW ====================*/
+// Array of image sources
+const images = [
+    'assets/images/Oncare-background.png',
+    'assets/images/Oncare-background.png',
+    'assets/images/Oncare-background.png',
+    // Add more image paths as needed
+  ];
+  
+  // The index of the current image
+  let currentIndex = 0;
+  
+  // Function to cycle to the next image
+  function nextImage() {
+    const imgElement = document.querySelector('.skills__img');
+    
+    // Fade out the current image
+    imgElement.style.opacity = 0;
+  
+    // Wait for the fade-out transition to finish before changing the image source
+    setTimeout(() => {
+      currentIndex = (currentIndex + 1) % images.length;
+      imgElement.src = images[currentIndex];
+  
+      // Fade in the new image
+      imgElement.style.opacity = 1;
+    }, 1500); // This timeout duration should match the CSS transition duration
+  }
+  
+  // Set the interval for changing images (e.g., 3000ms = 3 seconds)
+  setInterval(nextImage, 5000);
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () =>{
+    const scrollDown = window.scrollY
+
+  sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id'),
+              sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+        
+        if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+            sectionsClass.classList.add('active-link')
+        }else{
+            sectionsClass.classList.remove('active-link')
+        }                                                    
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2000,
+    delay: 200,
+//     reset: true
+});
+
+sr.reveal('.home__data, .about__img',{}); 
+sr.reveal('.home__img, .about__subtitle, .about__text',{delay: 400}); 
+sr.reveal('.home__social-icon',{ interval: 200}); 
+sr.reveal('.contact__input',{interval: 200}); 
