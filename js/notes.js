8309f59fde129f205e865d73d09fcb14682b143d
Abisoye Onanuga <102636953+dottymatrix@users.noreply.github.com>
2024-03-25 08:59:58 -0400

    Revert "configure flask app"
    This reverts commit a9554e06a66f773a2144b790de111410ed2585fc.

---
+++ js/notes.js
@@ -0,0 +1,11 @@
+function addPatientNote() {
+    // Logic to add a patient note
+    // Update the notes-dates container with a new link
+}
+
+function filterNotesByDate(date) {
+    // Logic to filter notes by date
+}
+
+document.getElementById('note-entry-form').addEventListener('submit', addPatientNote);
+document.getElementById('filter-options').addEventListener('change', filterNotesByDate);
